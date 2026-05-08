89f754d555d2e7b64509ff4a9539abcd0a3e62d9
Mohan Narayanapuram <mohanvenkat017@gmail.com>
2026-05-08 14:49:57 +0530

    fix email port 587

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -3,7 +3,9 @@ const fs         = require('fs');
 const path       = require('path');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
