b47b432d476a725497599d7d092f343a6b299421
Mohan Narayanapuram <mohanvenkat017@gmail.com>
2026-05-08 15:49:59 +0530

    fix: Gmail OAuth2 email with smtp

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -3,7 +3,10 @@ const fs   = require('fs');
 const path = require('path');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     type: 'OAuth2',
     user: process.env.EMAIL_USER,
